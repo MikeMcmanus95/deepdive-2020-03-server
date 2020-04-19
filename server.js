5217d77af0c51dd8de574d376d9f3df1edf26df0
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-04-19 15:49:59 -0400

    changed port

--- server.js
+++ server.js
@@ -18,7 +18,7 @@
 
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
